96386e7d1f8d376f324bd8c063fac5cf2046c43f
tds <2028893704@qq.com>
2019-01-31 13:39:37 +0800

    test

---
+++ src/main/java/HelloServlet.java
@@ -0,0 +1,19 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Date;
+
+@WebServlet(name = "HelloServlet")
+public class HelloServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+       doGet(request,response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("测试热部署");
+        response.getWriter().println("Hello World" + "\r\n" + new Date().toLocaleString());
+    }
+}
